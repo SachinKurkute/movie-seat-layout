c5e6ad29bdaade35ba756d9dee278781be79ab41
Sachin Kurkute <sachin.kurkute@gmail.com>
2017-12-05 18:29:31 +0530

    Example updated

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,20 +1,30 @@
 $(document).ready(function () {
 
     function loadGrid() {
-        var seatData = $('.inputBox').val(); //
+        var seatData = $('.inputBox').val();
+        var nuberOfSeat = $('.nuberOfSeat').val();
         seatData = JSON.parse(seatData);
 
         $('.selectMove').seatLayout({
             data: seatData,
-            numberOfSeat: 3,
+            showActionButtons:true,
+            classes : {
+                doneBtn : '',
+                cancelBtn : '',
+                row:'',
+                area:'',
+                screen:'',
+                seat:''
+            },
+            numberOfSeat: nuberOfSeat,
             callOnSeatRender: function (Obj) {
-                //modify seat object and return it if require;
+                //modify seat object if require and return it;
                 return Obj;
             },
-            callOnSeatSelect: function (_event, _data, _element) {
+            callOnSeatSelect: function (_event, _data, _selected, _element) {
                 console.log(_event);
                 console.log(_data);
-                console.log(_element);
+                console.log(_selected);
             },
             selectionDone: function (_array) {
                 console.log(_array);
